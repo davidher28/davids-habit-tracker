723899c217d24257e3350fe8e3e9f14d5928d14d
David H <davidher28@javerianacali.edu.co>
2024-03-05 13:30:15 +0100

    Install the class-transformer library to validate DTOs

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,13 @@
 import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
-import { Logger } from '@nestjs/common'
+import { Logger, ValidationPipe } from '@nestjs/common'
 
 const APP_PORT = 3010
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
+  // TODO: Review if it's worth it to implement validation pipes for each endpoint
+  app.useGlobalPipes(new ValidationPipe())
   await app.listen(APP_PORT)
 }
 
