03794d2ebf0fa9a993f7fc8d4c40a9934ee9e8d5
David H <davidher28@javerianacali.edu.co>
2024-03-17 18:40:35 +0100

    Fix Test Suite

--- src/application/command/habit/cancel-habit.handler.spec.ts
+++ src/application/command/habit/cancel-habit.handler.spec.ts
@@ -1,17 +1,23 @@
-import { UUId } from '../../../domain'
+import { Habit, UUId } from '../../../domain'
 import { HabitRepository } from '../../../domain/habit/habit.repository'
 import { CancelHabitHandler } from './cancel-habit.handler'
 import { CancelHabitCommand } from './cancel-habit.command'
 import { HabitNotFoundError } from './habit.not-found.error'
 import { InMemoryHabitRepository } from '../../../infrastructure/habit/habit.in-memory.repository'
+import { EventPublisher } from '@nestjs/cqrs'
+import { HabitMother } from '../../../../test/habit/habit.mother'
 
 describe('CancelHabitHandler', () => {
+  let habit: Habit
   let habitRepository: HabitRepository
   let handler: CancelHabitHandler
+  let publisher: EventPublisher
 
   beforeEach(async () => {
+    habit = HabitMother.create()
     habitRepository = new InMemoryHabitRepository()
-    handler = new CancelHabitHandler(habitRepository)
+    publisher = { mergeObjectContext: jest.fn().mockReturnValue(habit) } as any
+    handler = new CancelHabitHandler(habitRepository, publisher)
   })
 
   it('should throw an error if the habit does not exist', async () => {
